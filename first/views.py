800a6919d573d3f42ea67a14ec6af57eb06c79b1
mehran_marhex <mehran2bb@gmail.com>
2024-12-30 06:20:56 -0800

    second commit, just for test

--- first/views.py
+++ first/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import psutil
+import time
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, My name in Mehran and This is a Page of Test Project.")
+    cpu_percent = 0
+    for x in range(20):
+        cpu_percent = psutil.cpu_percent()
+        return HttpResponse(f"CPU Percent is => {cpu_percent}")
+        time.sleep(1)
